a33efee3b031359f3651a252fa57ce9559291275
Christopher Kuo <christopherkuo@Christophers-MacBook-Air.local>
2023-10-22 21:10:08 -0700

    added htmx page switching

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "src", "index.html"));
 });
 
+app.get("/projects", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "src", "projects.html"));
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
